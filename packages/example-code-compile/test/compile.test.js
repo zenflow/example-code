cbaf3ca70684f46a70d6c0e2966e9cbc05b15dc7
Matthew Francis Brunetti <zenflow87@gmail.com>
2018-04-05 05:44:21 -0300

    tests(compile): simplify

--- packages/example-code-compile/test/compile.test.js
+++ packages/example-code-compile/test/compile.test.js
@@ -6,44 +6,39 @@ const stripNewlines = str => str.replace(/\s*\n\s*/g, '')
 
 describe('compile', () => {
   it('#01 basic', async () => {
-    const html = stripNewlines(`
-<pre>
-  <code class="executable js">let [foo, bar] = [1, 2]</code>
-</pre>`)
-    const compiledHtml = compile({ html })
+    const compiledHtml = compile({
+      html: `<pre><code class="executable js">let [foo, bar] = [1, 2]</code></pre>`,
+    })
     expect(compiledHtml).toMatchSnapshot()
   })
 
   it('#02 only touches <code class="executable js"> elements', async () => {
-    const html = stripNewlines(`
-<pre>
-  <code class="js">let [foo, bar] = [1, 2]</code>
-  <code class="executable">let [foo, bar] = [1, 2]</code>
-  <code>let [foo, bar] = [1, 2]</code>
-  <p class="executable js">let [foo, bar] = [1, 2]</p>
-</pre>`)
-    const compiledHtml = compile({ html })
-    expect(compiledHtml).toEqual(html)
+    const originalHtml = stripNewlines(`<pre>
+      <code class="js">let [foo, bar] = [1, 2]</code>
+      <code class="executable">let [foo, bar] = [1, 2]</code>
+      <code>let [foo, bar] = [1, 2]</code>
+      <p class="executable js">let [foo, bar] = [1, 2]</p>
+    </pre>`)
+    const compiledHtml = compile({ html: originalHtml })
+    expect(compiledHtml).toEqual(originalHtml)
   })
 
   it('#03 accepts babel configuration', async () => {
-    const html = stripNewlines(`
-<pre>
-  <code class="executable js">let [foo, bar] = [1, 2]</code>
-</pre>`)
-    const babel = {
-      presets: [
-        [
-          require.resolve('babel-preset-env'),
-          {
-            targets: {
-              browsers: ['last 1 Chrome version'],
+    const compiledHtml = compile({
+      html: `<code class="executable js">let [foo, bar] = [1, 2]</code>`,
+      babel: {
+        presets: [
+          [
+            require.resolve('babel-preset-env'),
+            {
+              targets: {
+                browsers: ['last 1 Chrome version'],
+              },
             },
-          },
+          ],
         ],
-      ],
-    }
-    const compiledHtml = compile({ html, babel })
+      },
+    })
     expect(compiledHtml).not.toEqual(expect.stringContaining('var')) // last Chrome version supports let/const
     expect(compiledHtml).toEqual(
       expect.stringContaining('&quot;use strict&quot;;'), // babel should still add "use strict"
